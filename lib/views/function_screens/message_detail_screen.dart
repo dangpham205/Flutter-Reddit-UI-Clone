119d957b3acdfe40b8fbb3b5e9a6abcaa3e1e181
Dawnny <haidawng205@gmail.com>
2022-05-02 21:57:16 +0700

    message detail screen

--- lib/views/function_screens/message_detail_screen.dart
+++ lib/views/function_screens/message_detail_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:midterm_519h0277/constants/colors.dart';
 import 'package:midterm_519h0277/models/message.dart';
 
 class MessageDetailScreeen extends StatefulWidget {
@@ -19,6 +20,57 @@ class _MessageDetailScreeenState extends State<MessageDetailScreeen> {
         backgroundColor: Colors.white,
         elevation: 2,
         title: Text(widget.message.username, style: const TextStyle(color: Colors.black),),
+      ),
+      bottomSheet: Container(
+        color: Colors.white,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+          child: Expanded(
+            child: Container(                //chat box
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.vertical(
+                  bottom: Radius.circular(12.0),
+                  top: Radius.circular(12.0)
+                ),
+                color: seperateColor,
+              ),
+              padding: const EdgeInsets.symmetric(horizontal: 12),
+              child: const Expanded(
+                flex: 1,
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Replying to message',
+                    hintStyle: TextStyle(color: Colors.grey, fontSize: 12),
+                  ),
+                )
+              ),
+            ),
+          ),
+        ),
+      ),
+      body: Container(
+        color: seperateColor,
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+              color: Colors.white,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(widget.message.title, style: const TextStyle(fontSize: 14, fontWeight: FontWeight.bold),),
+                  const SizedBox(height: 16,),
+                  const Divider(color: dividerColor,),
+                  Text(widget.message.username, style: const TextStyle(color: redditColor),),
+                  const SizedBox(height: 4,),
+                  Text(widget.message.subtitle)
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
