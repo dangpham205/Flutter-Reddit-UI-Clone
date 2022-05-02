23be88bb562c0e997e13507b44fea14f4ae2479a
Dawnny <haidawng205@gmail.com>
2022-05-02 19:53:04 +0700

    notifi screen

--- lib/views/main_screens/notification_screen.dart
+++ lib/views/main_screens/notification_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:midterm_519h0277/views/main_screens/tab_screens.dart/inbox_messages.dart';
+import 'package:midterm_519h0277/views/main_screens/tab_screens.dart/inbox_notifications.dart';
 
 import '../../widgets/end_drawer.dart';
 import '../../widgets/start_drawer.dart';
@@ -13,26 +15,65 @@ class NotificationScreen extends StatefulWidget {
 class _NotificationScreenState extends State<NotificationScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        drawer: const StartDrawer(),
-        endDrawer: const EndDrawer(),
-        appBar: AppBar(
-          elevation: 0,
-          backgroundColor: Colors.white,
-          actions: [
-            Builder(
-              builder: (context) => TextButton(
-                onPressed: () {
-                  Scaffold.of(context).openEndDrawer();   //openenddrawer
-                }, 
-                child: const CircleAvatar(
-                  radius: 16,
-                  backgroundImage: AssetImage('assets/image/reddit_avatar.png'),
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+          drawer: const StartDrawer(),
+          endDrawer: const EndDrawer(),
+          appBar: AppBar(
+            elevation: 0,
+            backgroundColor: Colors.white,
+            title: const Text("Inbox", style: TextStyle(color: Colors.black),),
+            actions: [
+              Builder(
+                builder: (context) => TextButton(
+                  onPressed: () {
+                    Scaffold.of(context).openEndDrawer();   //openenddrawer
+                  }, 
+                  child: const CircleAvatar(
+                    radius: 16,
+                    backgroundImage: AssetImage('assets/image/reddit_avatar.png'),
+                  ),
                 ),
               ),
+            ],
+            bottom: TabBar(
+              tabs: [
+                const Tab(
+                  child: Text('Notifications', style: TextStyle(color: Colors.black),),
+                ),
+                Tab(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text('Messages', style: TextStyle(color: Colors.black),),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 8.0),
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(horizontal: 6,vertical: 2),
+                          decoration: const BoxDecoration(
+                            borderRadius: BorderRadius.vertical(
+                              bottom: Radius.circular(26.0),
+                              top: Radius.circular(26.0)
+                            ),
+                            color: Colors.red,
+                          ),
+                          child: const Text('3'),
+                        ),
+                      )
+                    ],
+                  ),
+                ),              
+              ],
             ),
-          ],
+          ),
+          body: const TabBarView(
+          children: [
+            NotificationsTab(),
+            MessagesTab()
+          ]
         ),
+      ),
     );
   }
 }
---
+++ lib/views/main_screens/tab_screens.dart/inbox_messages.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MessagesTab extends StatefulWidget {
+  const MessagesTab({ Key? key }) : super(key: key);
+
+  @override
+  State<MessagesTab> createState() => MessagesStateTab();
+}
+
+class MessagesStateTab extends State<MessagesTab> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
