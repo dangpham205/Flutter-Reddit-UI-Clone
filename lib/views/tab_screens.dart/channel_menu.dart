c94bde266ee41a43fc259d4ef58c818a72d2fe41
Dawnny <haidawng205@gmail.com>
2022-05-03 16:08:28 +0700

    channel screen done

--- lib/views/tab_screens.dart/channel_menu.dart
+++ lib/views/tab_screens.dart/channel_menu.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../constants/colors.dart';
+
 class ChannelMenuTab extends StatefulWidget {
   const ChannelMenuTab({ Key? key }) : super(key: key);
 
@@ -11,7 +13,49 @@ class _ChannelMenuTabState extends State<ChannelMenuTab> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      padding: const EdgeInsets.only(top:12, bottom: 12),
+      color: seperateColor,
+      child: Column(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            color: Colors.white,
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Text('Blog', style: TextStyle(color: textColor2, fontWeight: FontWeight.bold),),
+              ],
+            ),
+          ),
+          const SizedBox(height: 12,),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            color: Colors.white,
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Text('Contact Us', style: TextStyle(color: textColor2, fontWeight: FontWeight.bold),),
+              ],
+            ),
+          ),
+          const SizedBox(height: 12,),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            color: Colors.white,
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Text('Help', style: TextStyle(color: textColor2, fontWeight: FontWeight.bold),),
+              ],
+            ),
+          ),
+          const SizedBox(height: 12,),
+          
+        ]
+      )
     );
   }
 }
